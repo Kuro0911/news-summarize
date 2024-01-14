07b5196c37acce20bca625df72a51ca465d8416f
Kuro0911 <dhananjaisharma09@gmail.com>
2024-01-15 01:52:25 +0530

    init + frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Navbar } from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div className="hero min-h-[88vh] bg-base-200">
+        <div className="hero-content text-center">
+          <div className="max-w-full">
+            <h1 className="text-5xl font-bold">
+              Your <span className="text-primary">News</span>, Faster and
+              Smarter
+            </h1>
+            <div className="flex items-center justify-center h-[20vh] w-full">
+              <label className="form-control w-full justify-center items-center">
+                <div className="label w-3/4">
+                  <span className="label-text text-lg text-accent">
+                    Enter News Article URL
+                  </span>
+                </div>
+                <input
+                  type="text"
+                  placeholder="Type here"
+                  className="input input-bordered w-3/4"
+                />
+              </label>
+            </div>
+            <button className="btn w-1/2 btn-success h-12 text-lg">
+              Summarize Now
+            </button>
+
+            <p className="text-lg m-4">
+              QuickNews AI simplifies news reading by first{" "}
+              <span className="text-success">scraping content</span> from your
+              provided article link, then utilizing{" "}
+              <span className="text-success">advanced AI</span> to distill the
+              essence into a{" "}
+              <span className="text-success">concise summary</span>.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
